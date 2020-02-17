2610bd112265d4aeced5d73ce01c3841a460e41e
MASTERGUY <murtipipo@gmail.com>
2020-02-17 09:25:22 +0530

    DerpSite : Minified & Improvements for smartphones

--- js/search.js
+++ js/search.js
@@ -1,11 +1,16 @@
 var input = document.getElementById("sb");
 var entries = document.getElementsByClassName("device");
 
-input.addEventListener("input", function(){
+input.addEventListener(
+  "input",
+  function() {
     var val = input.value;
-    for (var i=0, l=entries.length; i<l; i++) {
-        var entryText = entries[i].getElementsByTagName("h4")[0].innerHTML;
-        if (entryText.toLowerCase().indexOf(val.toLowerCase()) != -1) entries[i].style.display = "block";
-        else entries[i].style.display = "none";
+    for (var i = 0, l = entries.length; i < l; i++) {
+      var entryText = entries[i].getElementsByTagName("h4")[0].innerHTML;
+      if (entryText.toLowerCase().indexOf(val.toLowerCase()) != -1)
+        entries[i].style.display = "block";
+      else entries[i].style.display = "none";
     }
-}, false)
+  },
+  false
+);
