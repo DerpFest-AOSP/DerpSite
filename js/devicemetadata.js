fbe3c447edeb32ae4b694a028828e134ba24011a
MASTERGUY <murtipipo@gmail.com>
2020-03-08 17:43:42 +0530

    DerpSite: Add Automatic Download Count * Fetches from SurceForge on per device basis
    Co-authored-by: nullxception <enuma.alrizky@gmail.com>
    Co-authored-by: idoybh <idoybh2@gmail.com>

--- js/devicemetadata.js
+++ js/devicemetadata.js
@@ -1,5 +1,6 @@
 var sourceForge = "https://sourceforge.net/projects/derpfest";
 var updatedb = "https://raw.githubusercontent.com/DerpLab/Updater-Stuff/master";
+var today = new Date().toISOString().slice(0, 10);
 var onPageLoad = function() {
   document.querySelectorAll("div[data-device]").forEach(el => {
     var device = el.getAttribute("data-device");
@@ -7,7 +8,16 @@ var onPageLoad = function() {
       .then(res => res.json())
       .then(ret => {
         var file = ret.response[0].filename;
-        el.getElementsByClassName("btn-download")[0].href = sourceForge + "/files/" + device + "/" + file + "/download";
+        file = sourceForge + "/files/" + device + "/" + file;
+        el.getElementsByClassName("btn-download")[0].href = file + "/download";
+        fetch(file + "/stats/json?start_date=2000-01-01" + "&end_date=" + today)
+          .then(res => res.json())
+          .then(ret => {
+            el.getElementsByClassName("total-download")[0].innerHTML = "Download Count</br>(Per latest build)<br>" + ret.total + "</br>";
+          })
+          .catch(err => {
+            console.error("failed to fetch total download count for " + device);
+          });
       })
       .catch(err => {
         console.error("failed to fetch " + device + " data");
