9f4d159598156d50a41ae5a30c1821adb14ba4b4
MASTERGUY <murtipipo@gmail.com>
2020-03-08 16:57:31 +0530

    DerpSite: Add per device download count * fetches total download count of the last build from SourceForge
    Co-authored-by: nullxception <enuma.alrizky@gmail.com>
    Co-authored-by: MASTERGUY <murtipipo@gmail.com>

--- js/devicemetadata.js
+++ js/devicemetadata.js
@@ -7,7 +7,18 @@ var onPageLoad = function() {
       .then(res => res.json())
       .then(ret => {
         var file = ret.response[0].filename;
-        el.getElementsByClassName("btn-download")[0].href = sourceForge + "/files/" + device + "/" + file + "/download";
+        file = sourceForge + "/files/" + device + "/" + file + "/download"
+        el.getElementsByClassName("btn-download")[0].href = file;
+        var today = new Date().toIOString().slice(0, 10);
+        fetch(file + "/stats/json?start_date=2000-01-01" + "&end_date=" + today")
+          .then(res => res.json())
+          .then(ret => {
+            var dlc = ret.response[0].summaries.time.downloads;
+            el.getElementsByClassName("lbl btn-download")[0].href = dlc;
+          })
+          .catch(err => {
+            console.error("failed to fetch " + device + " download count");
+          });
       })
       .catch(err => {
         console.error("failed to fetch " + device + " data");
