2d891ce3d11eb1f298a0335d765f335532b335db
MASTERGUY <murtipipo@gmail.com>
2020-06-15 11:08:06 +0530

    DerpSite: 1)Moved To New Download Servers Thanks to @CryllicBuster273 2)New Devices Added 3)Updated Weekly Changelogs
    Signed-off-by: MASTERGUY <murtipipo@gmail.com>

--- js/devicemetadata.js
+++ js/devicemetadata.js
@@ -1,4 +1,4 @@
-var sourceForge = "https://sourceforge.net/projects/derpfest";
+var gdindex = "https://get.derpfest.org/";
 var updatedb = "https://raw.githubusercontent.com/DerpLab/Updater-Stuff/master";
 var today = new Date().toISOString().slice(0, 10);
 var onPageLoad = function() {
@@ -8,16 +8,11 @@ var onPageLoad = function() {
       .then(res => res.json())
       .then(ret => {
         var file = ret.response[0].filename;
-        file = sourceForge + "/files/" + device + "/" + file;
-        el.getElementsByClassName("btn-download")[0].href = file + "/download";
-        fetch(file + "/stats/json?start_date=2000-01-01" + "&end_date=" + today)
-          .then(res => res.json())
-          .then(ret => {
-            el.getElementsByClassName("total-download")[0].innerHTML = "Download Count</br>(Per latest build)<br>" + ret.total + "</br>";
-          })
-          .catch(err => {
-            console.error("failed to fetch total download count for " + device);
-          });
+        var dl = gdindex + device + "/builds/"+ file;
+        el.getElementsByClassName("btn-download")[0].href = dl;
+        var md5 = gdindex + device + "/md5/"+ file+ ".md5sum";
+        el.getElementsByClassName("btn-md5")[0].href = md5;  
+          
       })
       .catch(err => {
         console.error("failed to fetch " + device + " data");
