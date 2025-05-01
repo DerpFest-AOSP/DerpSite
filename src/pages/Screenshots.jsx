e35cead5ecca09c45884a4462c9c68ddda1759f4
Hyeonho Kang <hyeonhokang10@kakao.com>
2025-05-01 21:43:51 +0900

    Add screenshots page

--- src/pages/Screenshots.jsx
+++ src/pages/Screenshots.jsx
@@ -2,6 +2,41 @@ const Screenshots = () => {
    return (
       <div className="py-6">
          <h1 className="text-4xl text-center font-bold">Screenshots</h1>
+         <div class="grid grid-cols-5 gap-4 p-6">
+            <div>
+               <img src="./img/screens/screen1.png" className="rounded-2xl transition hover:scale-105" />
+            </div>
+            <div>
+               <img src="./img/screens/screen2.png" className="rounded-2xl transition hover:scale-105" />
+            </div>
+            <div>
+               <img src="./img/screens/screen3.png" className="rounded-2xl transition hover:scale-105" />
+            </div>
+            <div>
+               <img src="./img/screens/screen4.png" className="rounded-2xl transition hover:scale-105" />
+            </div>
+            <div>
+               <img src="./img/screens/screen5.png" className="rounded-2xl transition hover:scale-105" />
+            </div>
+            <div>
+               <img src="./img/screens/screen6.png" className="rounded-2xl transition hover:scale-105" />
+            </div>
+            <div>
+               <img src="./img/screens/screen7.png" className="rounded-2xl transition hover:scale-105" />
+            </div>
+            <div>
+               <img src="./img/screens/screen8.png" className="rounded-2xl transition hover:scale-105" />
+            </div>
+            <div>
+               <img src="./img/screens/screen9.png" className="rounded-2xl transition hover:scale-105" />
+            </div>
+            <div>
+               <img src="./img/screens/screen10.png" className="rounded-2xl transition hover:scale-105" />
+            </div>
+            <div>
+               <img src="./img/screens/screen11.png" className="rounded-2xl transition hover:scale-105" />
+            </div>
+         </div>
       </div>
    );
 };
