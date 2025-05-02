7d7c8d7da75302bf35e75a4bd5a2fbb25b61b8b8
Hyeonho Kang <hyeonhokang10@kakao.com>
2025-05-02 22:08:09 +0900

    Add changelogs page

--- src/pages/Changelogs.jsx
+++ src/pages/Changelogs.jsx
@@ -2,6 +2,25 @@ const Changelogs = () => {
    return (
       <div className="py-6">
          <h1 className="text-4xl text-center font-bold">Changelogs</h1>
+         <div className="p-6 md:px-12 px-6 leading-8">
+            <div className="mb-3">
+               <h2 className="text-4xl mb-3">Monthly Changelogs - 14</h2>
+               <h4 className="text-2xl mb-3">Date: 07th August 2024</h4>
+               [*] Merge August security patches (android-14.0.0_r56) <br />
+               [*] Yaap quote: "Refactored the hell out of battery styles code <br />
+               [*] PixelPropsUtils: Updates from Rising OS <br />
+               [*] DerpLauncher: Integrate themed icons into the recents <br />
+               [*] DerpLauncher: Update themed icons <br />
+               [*] Flash: change to FlashMode.OFF before switching usecases <br />
+               [*] Update few translations <br />
+            </div>
+
+            <div className="mb-3">
+               <h2 className="text-4xl mb-3">Monthly Changelogs - 13</h2>
+               <h4 className="text-2xl mb-3">Date: 07th August 2024</h4>
+               [*] Merged August security patch <br />
+            </div>
+         </div>
       </div>
    );
 };
