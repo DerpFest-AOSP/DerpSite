86ead4029201d2d97e461d498267d8978c688582
Yaznas <sanjshrestha53@gmail.com>
2025-05-06 00:53:28 +0545

    DerpSite: Few Changes - hide 2 screenshots in homepage for mobile - add relevant meta tags to index.html - change homepage screenshots to Android 15

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,9 +19,9 @@ const Home = () => {
                      </Link>
                   </div>
                   <div className="flex justify-center pt-6">
-                     <img src="./img/product-screen-1.png" className="mt-24 w-80 h-fit mr-4 rounded-3xl animate__animated animate__fadeInUp" />
-                     <img src="./img/product-screen-2.png" className="mt-12 w-80 h-fit mr-4 rounded-3xl animate__animated animate__fadeInUp animate__faster" />
-                     <img src="./img/product-screen-3.png" className="w-80 h-fit rounded-3xl animate__animated animate__fadeInUp" />
+                     <img src="./img/product-screen-1.png" alt="Derpfest Wallpapers" className="mt-24 w-80 h-fit mr-4 rounded-3xl animate__animated animate__fadeInUp md:block hidden" />
+                     <img src="./img/product-screen-2.png" alt="Derpfest Customizations" className="mt-12 w-80 h-fit mr-4 rounded-3xl animate__animated animate__fadeInUp animate__faster md:block hidden" />
+                     <img src="./img/product-screen-3.png" alt="Derpfest About Phone" className="w-80 h-fit rounded-3xl animate__animated animate__fadeInUp" />
                   </div>
                </div>
             </div>
